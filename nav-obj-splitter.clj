ee2d284790f7f650f7de6a13e590cda71d1df826
Pascal Ullrich <tentacledev@gmail.com>
2012-10-16 13:59:13 +0200

    Changed tabs to spaces.

--- nav-obj-splitter.clj
+++ nav-obj-splitter.clj
@@ -8,29 +8,29 @@
 (re-find (re-pattern "^Table|Form|Report|Dataport|XMLport|Page|Codeunit") "Table 100 Test Table")
 
 (defn type-id [type]
-	(cond
-		(= type "Table") 1
-		(= type "Form") 2
-		(= type "Report") 3
-		(= type "Dataport") 4
-		(= type "Codeunit") 5
-		(= type "XMLport") 6
-		(= type "MenuSuite") 7
-		(= type "Page") 8))
+  (cond
+    (= type "Table") 1
+    (= type "Form") 2
+    (= type "Report") 3
+    (= type "Dataport") 4
+    (= type "Codeunit") 5
+    (= type "XMLport") 6
+    (= type "MenuSuite") 7
+    (= type "Page") 8))
 
 (defn parse-first-line [line]
-	(let [matches (re-find (re-pattern #"^(.+) (\d+) (.+)") line)
-		  type (nth matches 1)
-		  id (nth matches 2)
-		  name (nth matches 3)]
-		{:type-id (type-id type) :type type :id id :name name}))
+  (let [matches (re-find (re-pattern #"^(.+) (\d+) (.+)") line)
+        type (nth matches 1)
+        id (nth matches 2)
+        name (nth matches 3)]
+    {:type-id (type-id type) :type type :id id :name name}))
 
 (defn make-single-file-name [{type-id :type-id type :type id :id}]
-	"1-Table-100-Test Table")
-	
+  "1-Table-100-Test Table")
+
 (defn make-single-file-name []
-	(+ "1" "test"))
-	
+  (+ "1" "test"))
+
 (join ["1" "test"])
 (join "-" ["1" "test"])
 (parse-first-line "Table 100 Test Table")
