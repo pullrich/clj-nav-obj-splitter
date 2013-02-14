932f4f5304a280403218b8ecbf26dbed68c6d402
Pascal Ullrich <tentacledev@gmail.com>
2013-02-15 00:45:29 +0100

    Got rid of function call outside of tests.

--- nav_source_test.clj
+++ nav_source_test.clj
@@ -1,15 +1,16 @@
 (ns nav-source-test
-  (:use nav-source)
-  (:require [clojure.test :as test]))
+  (:use nav-source clojure.test))
 
 (def expected-metadata {:type-id 1 :type "Table" :id "70902" :name "Test Table (48)"})
 (def first-line-example "OBJECT Table 70902 Test Table (48)")
-(def meta-data-parsed (parse-first-line first-line-example))
 
-(test/deftest parsing-first-line-returns-map-of-named-elements
-  (test/is (= expected-metadata meta-data-parsed)))
+(deftest first-line-of-object-source?-should-return-true
+  (is (true? (first-line-of-object-source? first-line-example))))
 
-(test/deftest make-single-file-name-returns-uniquely-identifiable-filename
-  (test/is (= "1-Table-70902.txt" (make-single-file-name meta-data-parsed))))
+(deftest parsing-first-line-returns-map-of-named-elements
+  (is (= expected-metadata (parse-first-line first-line-example))))
 
-(test/run-tests 'nav-source-test)
+(deftest make-single-file-name-returns-uniquely-identifiable-filename
+  (is (= "1-Table-70902.txt" (make-single-file-name (parse-first-line first-line-example)))))
+
+(run-tests 'nav-source-test)
